c839d054835b4917f6e7e72e012552140b2962db
Kristiyan Tsaklev <cuki@cukii.me>
2020-12-28 13:32:24 +0200

    Do not pass locations around

--- src/Text/Json/Parser.hs
+++ src/Text/Json/Parser.hs
@@ -16,7 +16,7 @@ module Text.Json.Parser
   ) where
 
 import qualified Data.ByteString.Lazy.Char8 as BSL
-import Data.Bifunctor (second)
+import Data.Bifunctor (first, second)
 import Data.STRef
 import Control.Monad.ST
 
@@ -24,47 +24,47 @@ data Location
   = LocationArrayIndex !Int
   | LocationObjectKey !BSL.ByteString
 
-newtype Parser s a = Parser ([Location] -> STRef s BSL.ByteString -> ST s (Either (String, [Location]) a))
+newtype Parser s a = Parser (STRef s BSL.ByteString -> ST s (Either (String, [Location]) a))
 
 instance Functor (Parser s) where
-  fmap f (Parser parser) = Parser $ \loc ref -> second f <$> parser loc ref
+  fmap f (Parser parser) = Parser $ \ref -> second f <$> parser ref
 
 instance Applicative (Parser s) where
-  pure x = Parser $ \_ _ -> pure $ Right x
-  Parser p1 <*> Parser p2 = Parser $ \loc ref ->
-    p1 loc ref >>= \case
+  pure x = Parser $ \_ -> pure $ Right x
+  Parser p1 <*> Parser p2 = Parser $ \ref ->
+    p1 ref >>= \case
       Left e -> pure $ Left e
-      Right f -> p2 loc ref >>= \case
+      Right f -> p2 ref >>= \case
         Left e -> pure $ Left e
         Right x -> pure $ Right $ f x
 
 instance Monad (Parser s) where
-  Parser p >>= f = Parser $ \loc ref -> do
-    p loc ref >>= \case
+  Parser p >>= f = Parser $ \ref -> do
+    p ref >>= \case
       Left e -> pure $ Left e
       Right x -> do
         let Parser p2 = f x
-        p2 loc ref
+        p2 ref
 
 instance MonadFail (Parser s) where
-  fail msg = Parser $ \loc _ -> pure $ Left (msg, loc)
+  fail msg = Parser $ \_ -> pure $ Left (msg, [])
 
 runParser :: Parser s a -> BSL.ByteString -> ST s (Either (String, [Location]) a)
-runParser json input = parser [] =<< newSTRef input
+runParser json input = parser =<< newSTRef input
   where Parser parser = json <* eof
 
 eof :: Parser s ()
-eof = Parser $ \loc ref -> do
+eof = Parser $ \ref -> do
   input <- readSTRef ref
   pure $ if BSL.null input
     then Right ()
-    else Left ("Expected end of input", loc)
+    else Left ("Expected end of input", [])
 
 liftST :: ST s a -> Parser s a
-liftST action = Parser $ \_ _ -> fmap Right action
+liftST action = Parser $ \_ -> fmap Right action
 
 atLocation :: Location -> Parser s a -> Parser s a
-atLocation sub (Parser parser) = Parser $ parser . (sub:)
+atLocation sub (Parser parser) = Parser $ fmap (first (second (sub :))) . parser
 
 atArrayIndex :: Int -> Parser s a -> Parser s a
 atArrayIndex = atLocation . LocationArrayIndex
@@ -73,60 +73,60 @@ atObjectKey :: BSL.ByteString -> Parser s a -> Parser s a
 atObjectKey = atLocation . LocationObjectKey
 
 showError :: (String, [Location]) -> String
-showError (msg, locs) = "$" ++ concatMap showLocation (reverse locs) ++ ": " ++ msg
+showError (msg, locs) = "$" ++ concatMap showLocation locs ++ ": " ++ msg
   where
     showLocation = \case
       LocationArrayIndex index -> "[" ++ show index ++ "]"
       LocationObjectKey  key   -> "[" ++ show key   ++ "]"
 
 anyChar :: Parser s Char
-anyChar = Parser $ \loc ref -> do
+anyChar = Parser $ \ref -> do
   input <- readSTRef ref
   case BSL.uncons input of
-    Nothing -> pure $ Left ("Unexpected end of input", loc)
+    Nothing -> pure $ Left ("Unexpected end of input", [])
     Just (x, xs) -> do
       writeSTRef ref xs
       pure $ Right x
 
 char :: Char -> Parser s ()
-char c = Parser $ \loc ref -> do
+char c = Parser $ \ref -> do
   input <- readSTRef ref
   case BSL.uncons input of
-    Nothing -> pure $ Left ("Unexpected end of input", loc)
+    Nothing -> pure $ Left ("Unexpected end of input", [])
     Just (x, xs)
-      | c /= x -> pure $ Left ("Unexpected " ++ show x, loc)
+      | c /= x -> pure $ Left ("Unexpected " ++ show x, [])
       | otherwise -> do
           writeSTRef ref xs
           pure $ Right ()
 
 satisfy :: (Char -> Bool) -> Parser s Char
-satisfy f = Parser $ \loc ref -> do
+satisfy f = Parser $ \ref -> do
   input <- readSTRef ref
   case BSL.uncons input of
-    Nothing -> pure $ Left ("Unexpected end of input", loc)
+    Nothing -> pure $ Left ("Unexpected end of input", [])
     Just (x, xs)
       | f x -> do
           writeSTRef ref xs
           pure $ Right x
-      | otherwise -> pure $ Left ("Unexpected " ++ show x, loc)
+      | otherwise -> pure $ Left ("Unexpected " ++ show x, [])
 
 string :: BSL.ByteString -> Parser s ()
-string prefix = Parser $ \loc ref -> do
+string prefix = Parser $ \ref -> do
   input <- readSTRef ref
   case BSL.stripPrefix prefix input of
-    Nothing -> pure $ Left ("no match", loc)
+    Nothing -> pure $ Left ("no match", [])
     Just suffix -> do
       writeSTRef ref suffix
       pure $ Right ()
 
 takeWhileC :: (Char -> Bool) -> Parser s BSL.ByteString
-takeWhileC f = Parser $ \_ ref -> do
+takeWhileC f = Parser $ \ref -> do
   input <- readSTRef ref
   let (prefix, suffix) = BSL.span f input
   writeSTRef ref suffix
   pure $ Right prefix
 
 skipWhile :: (Char -> Bool) -> Parser s ()
-skipWhile f = Parser $ \_ ref -> do
+skipWhile f = Parser $ \ref -> do
   modifySTRef' ref $ BSL.dropWhile f
   pure $ Right ()
